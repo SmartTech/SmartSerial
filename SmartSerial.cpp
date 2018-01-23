6926c1051f3a4405ed8172202d0e0857181776d3
denis <denis_n73@mail.ru>
2018-01-23 08:36:09 +0700

    Add main files. Porting a library to public access

---
+++ SmartSerial.cpp
@@ -0,0 +1,290 @@
+/* ========================================================================
+ * SmartSerial - Arduino based library protocol with RS485 support
+ * ========================================================================
+ * The library consists of two classes:
+ *   SmartSSP - the low level driver and protocol implementation
+ *   SmartMSP - heir protocol implements the callback function
+ * ------------------------------------------------------------------------
+ * Features:
+ * - Support for the STM32 core Arduino
+ * - Support data filters by tags [MSP][DBG]
+ * - Support work on the RS485 interface
+ * - Flexible configuration callbacks
+ * - Ready-made templates of fixed data types: event, request, error & etc.
+ * - Transmission and reception of data volume limited by the RAM
+ * ------------------------------------------------------------------------
+ * License:
+ * GNU General Public License v3.0
+ * https://github.com/denisn73/SmartSerial/blob/master/LICENSE
+ * ------------------------------------------------------------------------
+ * Author:
+ * Developed by Denis Silivanov
+ * VK: @silivanov
+ * Instagram: @denisfpv
+ * Copyright (c) Denis Silivanov 2018
+ * https://github.com/denisn73/SmartSerial
+ * ------------------------------------------------------------------------
+ */
+
+#include "SmartSerial.h"
+
+#ifdef _VARIANT_ARDUINO_STM32_
+SmartSSP::SmartSSP(USBSerial* _serial) :
+  serial(_serial), _inputChar()
+{
+  construct();
+}
+#endif
+
+SmartSSP::SmartSSP(HardwareSerial* _serial, int pinTXen) :
+  Hardwareserial(_serial), serial(_serial), _pinTX(pinTXen), _inputChar()
+{
+  construct();
+  isHardwareSerial = true;
+}
+
+void SmartSSP::construct() {
+  inPacket.packetType = 0;
+  inPacket.nodeID     = 0;
+  inPacket.commandID  = 0;
+  inPacket.parity     = 0;
+  inPacket.datasize   = 0;
+  _inCounter          = 0;
+}
+
+/// Begin using default settings:
+///  - speed: 115200 baud
+///  - nodeID: 0
+void SmartSSP::begin() {
+  begin(DEFAULT_BAUDRATE, 0);
+}
+
+/// Begin using custom settings
+void SmartSSP::begin(long baud, uint8_t nodeID) {
+  if(isHardwareSerial) Hardwareserial->begin(_baud=baud);
+  else Serial.begin(baud);
+  setNodeID(nodeID);
+  if(_pinTX != PIN_UNCONNECTED) {
+	  pinMode(_pinTX, OUTPUT);
+	  digitalWrite(_pinTX, LOW);
+  }
+}
+
+// 
+void SmartSSP::setCallbackTimeout(uint16_t _timeout) {
+  _callbackTimeout = _timeout;
+}
+
+// 
+void SmartSSP::setAnswerTimeout(uint16_t _timeout) {
+  _answerTimeout = _timeout;
+}
+
+// Send outPacket
+void SmartSSP::sendPacket() {
+  
+  outPacket.parity  = outPacket.packetType;
+  outPacket.parity ^= outPacket.nodeID;
+  outPacket.parity ^= outPacket.commandID;
+  outPacket.parity ^= outPacket.datasize;
+  for(int i=0; i<outPacket.datasize; i++) {
+    outPacket.parity ^= outPacket.payload[i];
+  }
+  
+  enableTX();
+  //serial->println();
+  serial->print(TAG_MSP);
+  serial->print(TAG_TYPE);  hexPrinting(outPacket.packetType);
+  serial->print(TAG_NODE);  hexPrinting(outPacket.nodeID);
+  serial->print(TAG_CMD);   hexPrinting(outPacket.commandID);
+  serial->print(TAG_SIZE);  hexPrinting(outPacket.datasize);
+  serial->print(TAG_DATA);  for(int i=0; i<outPacket.datasize; i++) hexPrinting(outPacket.payload[i]);
+  serial->print(TAG_CRC);   hexPrinting(outPacket.parity);
+  serial->println();
+  
+}
+
+/// 
+bool SmartSSP::handle() {
+  static uint8_t processDataFlag = false;
+  if(isTX() && (micros() > _txMicros)) disableTX();
+  _ready = false;
+  while(serial->available()) {
+    char inChar = (char) serial->read();
+    if(inChar != '\n') {
+      if(inChar != '\r') {
+        if(_inCounter<5) _checkMSP += inChar;
+        else _inputChar[_inCounter-5] = inChar;
+        _inCounter++;
+      }
+    } else {
+      if(_checkMSP.equals(TAG_MSP)) {
+        if(parseData()) {
+          //serial->flush();
+		  if(_callbackTimeout) {
+			  processDataFlag = true;
+			  _callbackTimeoutMicros = micros() + _callbackTimeout;
+		  } else processData();
+          _ready     = true;
+          _checkMSP  = "";
+          _inCounter = 0;
+          return _ready;
+        }
+        else { // Parse packet error
+		  error();
+		  if(_errorUsage) sendError();
+        }
+      } else {
+		  
+	  }
+      _checkMSP = "";
+      _inCounter = 0;
+    }
+  }
+  if(processDataFlag && (micros() >= _callbackTimeoutMicros)) {
+	  processDataFlag = false;
+      processData();
+  }
+  handler();
+  return _ready;
+}
+
+void SmartSSP::processData() {
+  long _payload;
+  switch(inPacket.packetType) {
+    case TYPE_REPLY :
+      break;
+    case TYPE_ERROR :
+	  sendPacket();
+      break;
+    case TYPE_EVENT :
+      _payload  = inPacket.payload[0] << 24;
+      _payload += inPacket.payload[1] << 16;
+      _payload += inPacket.payload[2] << 8;
+      _payload += inPacket.payload[3];
+	  event(inPacket.commandID, _payload);
+      break;
+    case TYPE_ARRAY :
+	  array(inPacket.commandID, inPacket.payload, inPacket.datasize);
+      break;
+    case TYPE_VALUE :
+	  _payload  = inPacket.payload[0] << 24;
+      _payload += inPacket.payload[1] << 16;
+      _payload += inPacket.payload[2] << 8;
+      _payload += inPacket.payload[3];
+	  value(inPacket.commandID, _payload);
+      break;
+    case TYPE_REQUEST :
+	  request(inPacket.commandID);
+      break;
+    case TYPE_RESET :
+	  reset();
+      break;
+    default : break;
+  }
+  
+}
+
+bool SmartSSP::parseData() {
+  if(_inputChar[0] != 'T') return false;
+  _checkedParity = 0;
+  _checkedParity ^= inPacket.packetType = hex_to_dec(_inputChar[1])*16  + hex_to_dec(_inputChar[2]);
+  _checkedParity ^= inPacket.nodeID     = hex_to_dec(_inputChar[4])*16  + hex_to_dec(_inputChar[5]);
+  _checkedParity ^= inPacket.commandID  = hex_to_dec(_inputChar[7])*16  + hex_to_dec(_inputChar[8]);
+  _checkedParity ^= inPacket.datasize   = hex_to_dec(_inputChar[10])*16 + hex_to_dec(_inputChar[11]);
+  if(inPacket.payload) delete[] inPacket.payload;
+  inPacket.payload = new uint8_t[inPacket.datasize];
+  for(int i=0; i<inPacket.datasize; i++) {
+	unsigned int indexPayload = i*2+13;
+	inPacket.payload[i] = hex_to_dec(_inputChar[indexPayload])*16 + hex_to_dec(_inputChar[indexPayload+1]);
+    _checkedParity ^= inPacket.payload[i];
+  }
+  unsigned int indexParity = inPacket.datasize*2+14;
+  inPacket.parity = hex_to_dec(_inputChar[indexParity])*16 + hex_to_dec(_inputChar[indexParity+1]);
+  #ifdef DEBUG_SERIAL
+    printInfo();
+  #endif
+  return (_checkedParity == inPacket.parity);
+}
+
+/// 
+bool SmartSSP::available() {
+  return serial->available();
+}
+
+/// HexPrinting: helper function to print data with a constant field width (1 hex values)
+void SmartSSP::hexPrinting(uint8_t& data) {
+  if(data<16) serial->print(0, HEX);
+  serial->print(data, HEX);
+}
+
+/// HexPrinting: helper function to print data with a constant field width (2 hex values)
+void SmartSSP::hexPrinting(int16_t& data) {
+  if(data<4096) serial->print(0, HEX);
+  if(data<256)  serial->print(0, HEX);
+  if(data<16)   serial->print(0, HEX);
+  serial->print(uint16_t(data), HEX);              // casting to suppress FFFF for negative int values
+}
+
+/// Convert HEX to Decimal
+#define HEX_DEC_ERROR 0
+uint8_t SmartSSP::hex_to_dec(uint8_t in) {
+  if(((in >= '0') && (in <= '9'))) return in-'0';
+  in |= 0x20;
+  if(((in >= 'a') && (in <= 'f'))) return in-'a' + 10;
+  return HEX_DEC_ERROR;
+}
+
+/// printInfo:
+void SmartSSP::printInfo() {
+#ifdef DEBUG_SERIAL
+  printlnDebug("[DBG] >>> inPacket info <<<");
+  printDebug  ("[DBG] Type:      "); printlnDebug(inPacket.packetType, HEX);
+  printDebug  ("[DBG] NodeID:    "); printlnDebug(inPacket.nodeID,     HEX);
+  printDebug  ("[DBG] CommandID: "); printlnDebug(inPacket.commandID,  HEX);
+  printDebug  ("[DBG] Datasize:  "); printlnDebug(inPacket.datasize,   HEX);
+  printDebug  ("[DBG] Payload:   "); printlnDebug("...");//printlnDebug(inPacket.payload,HEX);
+  printDebug  ("[DBG] Parity:    "); printlnDebug(inPacket.parity,     HEX);
+  printDebug  ("[DBG] Checked:   "); printlnDebug(_checkedParity,      HEX);
+#endif
+}
+
+/// Set nodeID
+void SmartSSP::setNodeID(uint8_t& nodeID) {
+  outPacket.nodeID = nodeID;
+}
+
+/// Set commandID
+void SmartSSP::setCommandID(uint8_t& commandID) {
+  outPacket.commandID = commandID;
+}
+
+/// Set packet type
+void SmartSSP::setPacketType(uint8_t type) {
+  outPacket.packetType = type;
+}
+
+/// Get packet type
+uint8_t SmartSSP::getSize() {
+  return inPacket.datasize;
+}
+
+/// Get packet type
+uint8_t SmartSSP::getPacketType() {
+  return inPacket.packetType;
+}
+
+/// Get commandID
+uint8_t SmartSSP::getCommandID() {
+  return inPacket.commandID;
+}
+
+/// Get getPayload
+uint8_t SmartSSP::getPayload(byte num) {
+  return inPacket.payload[num];
+}
+
+/// Get getPayload
+uint8_t* SmartSSP::getPayload() {
+  return &inPacket.payload[0];
+}
---
+++ SmartSerial.h
@@ -0,0 +1,367 @@
+/* ========================================================================
+ * SmartSerial - Arduino based library protocol with RS485 support
+ * ========================================================================
+ * The library consists of two classes:
+ *   SmartSSP - the low level driver and protocol implementation
+ *   SmartMSP - heir protocol implements the callback function
+ * ------------------------------------------------------------------------
+ * Features:
+ * - Support for the STM32 core Arduino
+ * - Support data filters by tags [MSP][DBG]
+ * - Support work on the RS485 interface
+ * - Flexible configuration callbacks
+ * - Ready-made templates of fixed data types: event, request, error & etc.
+ * - Transmission and reception of data volume limited by the RAM
+ * ------------------------------------------------------------------------
+ * License:
+ * GNU General Public License v3.0
+ * https://github.com/denisn73/SmartSerial/blob/master/LICENSE
+ * ------------------------------------------------------------------------
+ * Author:
+ * Developed by Denis Silivanov
+ * VK: @silivanov
+ * Instagram: @denisfpv
+ * Copyright (c) Denis Silivanov 2018
+ * https://github.com/denisn73/SmartSerial
+ * ------------------------------------------------------------------------
+ */
+
+#ifndef _SMART_SERIAL_H_
+#define _SMART_SERIAL_H_
+
+#include <Arduino.h>
+
+//#define DEBUG_SERIAL
+#ifdef DEBUG_SERIAL
+#include <SmartDebug.h>
+#endif
+
+#define PIN_UNCONNECTED      -1
+
+#define DEFAULT_BAUDRATE 115200
+
+// status
+#define CONNECTED          true
+#define DISCONNECTED      false
+
+// tags:
+#define TAG_DBG         "[DBG] "
+#define TAG_MSP         "[MSP]"
+#define TAG_TYPE            "T"
+#define TAG_NODE            "N"
+#define TAG_CMD             "I"
+#define TAG_SIZE            "S"
+#define TAG_DATA            "P"
+#define TAG_CRC             "Q"
+
+// packet types:
+#define TYPE_REPLY         0x01
+#define TYPE_EVENT         0x02
+#define TYPE_ARRAY         0x03
+#define TYPE_VALUE         0x04
+#define TYPE_REQUEST       0x05
+#define TYPE_ERROR         0x07
+#define TYPE_RESET         0x1F
+
+#ifndef nullptr
+#define nullptr            0x00
+#endif
+
+// -------------------------------------------
+// SmartSSP - Smart Serial Protocol
+// -------------------------------------------
+class SmartSSP {
+  
+  private:
+    void construct();
+    bool isHardwareSerial = false;
+    HardwareSerial* Hardwareserial;
+    Stream*         serial;
+		
+    bool    parseData();
+    void    processData();
+    void    sendPacket();
+    void    printInfo();
+    void    hexPrinting(uint8_t& data);
+    void    hexPrinting(int16_t& data);
+    uint8_t hex_to_dec(uint8_t in);
+    
+    void    setPacketType(uint8_t type);
+    void    setCommandID(uint8_t& commandID);
+    void    setSensorID(uint8_t& sensorID);
+    void    setNodeID(uint8_t& nodeID);
+	    
+    struct Packet {
+      uint8_t  packetType;
+      uint8_t  nodeID;
+      uint8_t  commandID;
+      uint8_t  datasize;
+      uint8_t  parity;
+	  uint8_t* payload = nullptr;
+    } inPacket, outPacket;
+    
+	int      _pinTX = PIN_UNCONNECTED;
+    char     _inputChar[100];
+    uint8_t  _inCounter;
+    uint8_t  _checkedParity;
+    String   _checkMSP              = "";
+    bool     _ready                 = false;
+	bool     _txEnabled             = false;
+	uint16_t _callbackTimeout       = false;
+	uint16_t _answerTimeout         = false;
+	uint32_t _txMicros              = false;
+	uint32_t _baud                  = DEFAULT_BAUDRATE;
+	uint32_t _callbackTimeoutMicros = false;
+	uint32_t _answerTimeoutMicros   = false;
+    bool     _errorUsage            = true;
+	uint8_t  _isDebug               = false;
+	
+	virtual void request(int) {}
+	virtual void value(int, int) {}
+	virtual void array(int, uint8_t*, int) {}
+	virtual void event(int, int) {}
+	virtual void error() {}
+	virtual void reset() {}
+	virtual void handler() {}
+	
+	void enableTX() {
+		if(_pinTX != PIN_UNCONNECTED) {
+			digitalWrite(_pinTX, _txEnabled=HIGH);
+			uint16_t outDataSize = (outPacket.datasize*2) + strlen(TAG_MSP) + 20;
+			uint16_t packetMicros = (10000000 / _baud) * outDataSize;
+                        if(micros()<_txMicros) _txMicros += packetMicros;
+                        else _txMicros = micros() + packetMicros;
+		}
+	}
+	
+	void disableTX() {
+		if(_pinTX != PIN_UNCONNECTED) digitalWrite(_pinTX, _txEnabled=LOW);
+	}
+	
+    void sendError() {
+      if(outPacket.payload) delete[] outPacket.payload;
+      outPacket.packetType = TYPE_ERROR;
+      outPacket.commandID  = 0;
+      outPacket.datasize   = 1;
+      outPacket.datasize   = 1;
+	  outPacket.payload = new uint8_t[1];
+      outPacket.payload[0] = 0;
+      sendPacket();
+    }
+		
+  public:
+  
+    SmartSSP(HardwareSerial* _serial, int pinTXen = PIN_UNCONNECTED);
+    #ifdef _VARIANT_ARDUINO_STM32_
+      SmartSSP(USBSerial*    _serial);
+    #endif
+	
+    void     begin();
+    void     begin(long baud, uint8_t nodeID = 0);
+    bool     handle();
+	void     setCallbackTimeout(uint16_t _timeout);
+	void     setAnswerTimeout(uint16_t _timeout);
+	    
+    bool     available();
+    uint8_t  getDataID();
+    uint8_t  getCommandID();
+    uint8_t  getPacketType();
+    uint8_t  getSize();
+    uint8_t  getPayload(byte num);
+    uint8_t* getPayload();
+	
+	uint8_t isTX() {
+		return _txEnabled;
+	}
+	
+	void setErrorUsage(uint8_t _state) {
+		_errorUsage = _state;
+	}
+    
+    template < typename T >
+    void sendData(uint8_t  dataID, T dataArray, uint8_t length) {
+      if(outPacket.payload) delete[] outPacket.payload;
+      outPacket.packetType = TYPE_ARRAY;
+      outPacket.commandID  = dataID;
+      outPacket.datasize   = length;
+	  outPacket.payload    = new uint8_t[length];
+      for(int i=0 ; i<length ; i++) {
+        outPacket.payload[i] = ((uint8_t*)dataArray)[i];
+      }
+      sendPacket();
+    }
+    
+    template < typename T >
+    void sendData(uint8_t  dataID, T payload) {
+      if(outPacket.payload) delete[] outPacket.payload;
+      outPacket.packetType = TYPE_VALUE;
+      outPacket.commandID  = dataID;
+      outPacket.datasize   = 4;
+	  outPacket.payload = new uint8_t[4];
+      outPacket.payload[0] = payload >> 24;
+      outPacket.payload[1] = payload >> 16;
+      outPacket.payload[2] = payload >> 8;
+      outPacket.payload[3] = payload >> 0;
+      sendPacket();
+    }
+    
+    template < typename T >
+    void sendCommand(uint8_t dataID, T payload) {
+      if(outPacket.payload) delete[] outPacket.payload;
+      outPacket.packetType = TYPE_EVENT;
+      outPacket.commandID  = dataID;
+      outPacket.datasize   = 4;
+	  outPacket.payload = new uint8_t[4];
+      outPacket.payload[0] = payload >> 24;
+      outPacket.payload[1] = payload >> 16;
+      outPacket.payload[2] = payload >> 8;
+      outPacket.payload[3] = payload >> 0;
+      sendPacket();
+    }
+	
+	void sendRequast(uint8_t dataID) {
+	  if(outPacket.payload) delete[] outPacket.payload;
+      outPacket.packetType = TYPE_REQUEST;
+      outPacket.commandID  = dataID;
+      outPacket.datasize   = 1;
+	  outPacket.payload    = new uint8_t[1];
+      outPacket.payload[0] = 0;
+      sendPacket();
+	  if(_answerTimeout) {
+		  _answerTimeoutMicros = micros() + _answerTimeout;
+	  }
+	}
+	
+	void sendReset() {
+      if(outPacket.payload) delete[] outPacket.payload;
+      outPacket.packetType = TYPE_RESET;
+      outPacket.commandID  = 0;
+      outPacket.datasize   = 1;
+	  outPacket.payload    = new uint8_t[1];
+      outPacket.payload[0] = 0;
+      sendPacket();
+    }
+	    
+    //////////////////////////////////////////////////////////////
+	
+	void setDebug(uint8_t debugState) {
+		_isDebug = debugState;
+	}
+
+    void    simplePrint() {}   
+    template<class... T>
+    void simplePrint(T... args) {
+		if(!_isDebug) return;
+        serial->print(args...);
+    }
+	
+	template<class F, class S, class... T >
+	void debug(F arg, S index, T... args) {
+		if(!_isDebug) return;
+        serial->print(TAG_DBG);
+        serial->print(arg);
+        serial->print("[");
+        serial->print(index);
+        serial->print("]");
+        if(sizeof...(args) > 0) {
+          serial->print(": ");
+          simplePrint(args...);
+        }
+        serial->println();
+    };
+    
+    template<class F, class... T>
+    void debug(F arg, T... args) {
+		if(!_isDebug) return;
+        serial->print(TAG_DBG);
+        serial->print(arg);
+        if(sizeof...(args) > 0) {
+          serial->print(": ");
+          simplePrint(args...);
+        }
+        serial->println();
+    };
+	
+	template<class F>
+    void debug(F arg, uint32_t size) {
+		if(!_isDebug) return;
+        serial->print(TAG_DBG);
+        serial->println(arg, size);
+    };
+    
+    template<class... T>
+    void error(T... args) {
+      serial->print(TAG_DBG);
+      serial->print("error: ");
+      serial->println(args...);
+    };
+
+};
+
+// -------------------------------------------
+// SmartMSP - Smart Main Serial Protocol
+// is extended functions of SmartSSP
+// -------------------------------------------
+class SmartMSP : public SmartSSP {
+		
+	private :
+	
+    void (*user_onRequest)(int) = nullptr;
+    void (*user_onArray)(int, uint8_t*, int) = nullptr;
+    void (*user_onEvent)(int, int) = nullptr;
+    void (*user_onValue)(int, int) = nullptr;
+    void (*user_onError)() = nullptr;
+    void (*user_onReset)() = nullptr;
+	
+	void request(int id) override {
+		if (user_onRequest) user_onRequest(id);
+	}
+	void value(int id, int payload) override {
+		if (user_onValue) user_onValue(id, payload);
+	}
+	void array(int id, uint8_t* payload, int size) override {
+		if (user_onArray) user_onArray(id, payload, size);
+	}
+	void event(int id, int value) override {
+		if (user_onEvent) user_onEvent(id, value);
+	}
+	void error() override {
+		if (user_onError) user_onError();
+	}
+	void reset() override {
+		if (user_onReset) user_onReset();
+	}
+	
+	public :
+	
+	SmartMSP(HardwareSerial* _serial, int pinTXen = PIN_UNCONNECTED) : SmartSSP(_serial, pinTXen) {}
+    #ifdef _VARIANT_ARDUINO_STM32_
+      SmartMSP(USBSerial*    _serial) : SmartSSP(_serial) {}
+    #endif
+	
+    void attachRequest(void (*function)(int)) { user_onRequest = function; }
+    void attachValue(void (*function)(int, int)) { user_onValue = function; }
+    void attachArray(void (*function)(int, uint8_t*, int)) { user_onArray = function; }
+    void attachEvent(void (*function)(int, int)) { user_onEvent = function; }
+    void attachError(void (*function)()) { user_onError = function; }
+    void attachReset(void (*function)()) { user_onReset = function; }
+	
+	
+};
+
+// -------------------------------------------
+// === SSP protocol description ===
+// -------------------------------------------
+/* T01N00I01S01P05Q1A\r\n
+ * [T][01][N][00][I][01][S][FF][P][0000...01][Q][1A][\r][\n]
+ * "T" - packet type (byte, int, array, event, requast)
+ * "N" - nodeID      (remote device)
+ * "I" - commandID   ()
+ * "S" - datasize    (size of data)
+ * "P" - payload     (data)
+ * "Q" - parity      (crc)
+ * 
+ */
+// -------------------------------------------
+
+#endif // _SMART_SERIAL_H_
